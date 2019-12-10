83f37da4c0c04d2f5ee5d241ee3edec6cc6282c1
Richard Booth <richardabooth@gmail.com>
2019-12-10 08:32:10 +0000

    Added hankel test

--- frankenstein/tests.py
+++ frankenstein/tests.py
@@ -1,11 +1,72 @@
 """ Tests """
 
+import numpy as np
+
 # use relative imports since it is a package
 from .hankel import DiscreteHankelTransform
 
 
+def test_hankel_gauss():
+    """Check the Hankel Transform"""
+
+    def gauss_real_space(r):
+        x = r
+        return np.exp(-0.5 * x * x)
+
+    def gauss_vis_space(q):
+        qs = (2*np.pi) * q
+        return np.exp(-0.5 * qs * qs) * (2*np.pi)
+
+    DHT = DiscreteHankelTransform(5.0, 100)
+
+    Ir = gauss_real_space(DHT.r)
+    Iq = gauss_vis_space(DHT.q)
+
+    # Test at the DHT points
+    # Use a large error estimate because the DHT is approximate.
+    np.testing.assert_allclose(Iq, DHT.transform(Ir, direction='forward'),
+                               atol=1e-5, rtol=0, err_msg="Forward DHT")
+
+    np.testing.assert_allclose(Ir, DHT.transform(Iq, direction='backward'),
+                               atol=1e-5, rtol=0, err_msg="Inverse DHT")
+
+    # Test at generic points:
+    #   Larger Error needed
+    q = np.linspace(0.0, 1.0, 25)
+    np.testing.assert_allclose(gauss_vis_space(q),
+                               DHT.transform(Ir, q=q, direction='forward'),
+                               atol=1e-4, rtol=0, err_msg="Generic Forward DHT")
+
+    r = np.linspace(0, 5.0, 25)
+    np.testing.assert_allclose(gauss_real_space(r),
+                               DHT.transform(Iq, q=r, direction='backward'),
+                               atol=1e-4, rtol=0, err_msg="Generic Inverse DHT")
+
+    # Finally check the coefficients matrix works
+    Hf = DHT.coefficients(direction='forward')
+    Hb = DHT.coefficients(direction='backward')
+
+    np.testing.assert_allclose(np.dot(Hf, Ir),
+                               DHT.transform(Ir, direction='forward'),
+                               rtol=1e-8, err_msg="Forward DHT Coeffs")
+    np.testing.assert_allclose(np.dot(Hb, Iq),
+                               DHT.transform(Iq, direction='backward'),
+                               rtol=1e-8, err_msg="Inverse DHT Coeffs")
+
+    # Compare Cached vs non-cached DHT points:
+    np.testing.assert_allclose(DHT.coefficients(q=DHT.q),
+                               DHT.coefficients(),
+                               atol=1e-15, err_msg="Cached forward DHT Coeffs")
+    np.testing.assert_allclose(DHT.coefficients(q=DHT.r, direction='backward'),
+                               DHT.coefficients(direction='backward'),
+                               atol=1e-15, err_msg="Cached inverse DHT Coeffs")
+
 def test_import_data():
     # TODO
+    pass
+
+
+def test_fit_geometry():
     pass
 
 
