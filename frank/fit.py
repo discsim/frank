332f3b6ffabbdc53eb0bb293ce5fccabf9aa7782
Richard Booth <richardabooth@gmail.com>
2020-02-21 10:19:03 +0000

    Made doc-string format consistent with the rest of the code

--- frank/fit.py
+++ frank/fit.py
@@ -53,16 +53,16 @@ def parse_parameters():
     Parameters
     ----------
     parameter_filename : string, default `default_parameters.json`
-            Parameter file (.json; see frank.fit.helper)
+        Parameter file (.json; see frank.fit.helper)
     uvtable_filename : string
-            UVTable file with data to be fit (.txt, .dat, .npy, or .npz).
-            The UVTable column format should be:
-            u [lambda] v [lambda] Re(V) [Jy] Im(V) [Jy] Weight [Jy^-2]
+        UVTable file with data to be fit (.txt, .dat, .npy, or .npz).
+        The UVTable column format should be:
+        u [lambda] v [lambda] Re(V) [Jy] Im(V) [Jy] Weight [Jy^-2]
 
     Returns
     -------
     model : dict
-            Dictionary containing model parameters the fit uses
+        Dictionary containing model parameters the fit uses
     """
 
     import argparse
@@ -126,18 +126,18 @@ def load_data(data_file):
     Parameters
     ----------
     data_file : string
-          UVTable with columns:
-          u [lambda]  v [lambda]  Re(V) [Jy]  Im(V) [Jy] Weight [Jy^-2]
+        UVTable with columns:
+        u [lambda]  v [lambda]  Re(V) [Jy]  Im(V) [Jy] Weight [Jy^-2]
 
     Returns
     -------
     u, v : array, unit = :math:`\lambda`
-          u and v coordinates of observations
+        u and v coordinates of observations
     vis : array, unit = Jy
-          Observed visibilities (complex: real + imag * 1j)
+        Observed visibilities (complex: real + imag * 1j)
     weights : array, unit = Jy^-2
-          Weights assigned to observed visibilities, of the form
-          :math:`1 / \sigma^2`
+        Weights assigned to observed visibilities, of the form
+        :math:`1 / \sigma^2`
     """
     logging.info('  Loading UVTable')
     u, v, vis, weights = io.load_uvtable(data_file)
@@ -231,43 +231,43 @@ def perform_fit(u, v, vis, weights, geom, rout, n, alpha, wsmooth, max_iter,
     Parameters
     ----------
     u, v : array, unit = :math:`\lambda`
-          u and v coordinates of observations
+        u and v coordinates of observations
     vis : array, unit = Jy
-          Observed visibilities (complex: real + imag * 1j)
+        Observed visibilities (complex: real + imag * 1j)
     weights : array, unit = Jy^-2
-          Weights assigned to observed visibilities, of the form
-          :math:`1 / \sigma^2`
+        Weights assigned to observed visibilities, of the form
+        :math:`1 / \sigma^2`
     geom : SourceGeometry object
-          Fitted geometry (see frank.geometry.SourceGeometry)
+        Fitted geometry (see frank.geometry.SourceGeometry)
     rout : float, unit = arcsec
-          Maximum disc radius in the fit (best to overestimate size of source)
+        Maximum disc radius in the fit (best to overestimate size of source)
     n : int
-          Number of collocation points used in the fit
-          (suggested range 100 - 300)
+        Number of collocation points used in the fit
+        (suggested range 100 - 300)
     alpha : float
-          Order parameter for the power spectrum's inverse Gamma prior
-          (suggested range 1.00 - 1.50)
+        Order parameter for the power spectrum's inverse Gamma prior
+        (suggested range 1.00 - 1.50)
     wsmooth : float
-          Strength of smoothing applied to the power spectrum
-          (suggested range 10^-4 - 10^-1)
+        Strength of smoothing applied to the power spectrum
+        (suggested range 10^-4 - 10^-1)
     max_iter : int
-          Maximum number of fit iterations
+        Maximum number of fit iterations
     return_iteration_diag : bool
-          Whether to return diagnostics of the fit iteration
-          (see radial_fitters.FrankFitter.fit)
+        Whether to return diagnostics of the fit iteration
+        (see radial_fitters.FrankFitter.fit)
     diag_plot : bool
-          A check for whether to return diagnostics of the fit iteration
-          (if frank.make_figs.make_diag_fig is being called,
-          return_iteration_diag must be True)
+        A check for whether to return diagnostics of the fit iteration
+        (if frank.make_figs.make_diag_fig is being called,
+        return_iteration_diag must be True)
 
     Returns
     -------
     sol : _HankelRegressor object
-          Reconstructed profile using Maximum a posteriori power spectrum
-          (see frank.radial_fitters.FrankFitter)
+        Reconstructed profile using Maximum a posteriori power spectrum
+        (see frank.radial_fitters.FrankFitter)
     iteration_diag : _HankelRegressor object
-          Diagnostics of the fit iteration
-          (see radial_fitters.FrankFitter.fit)
+        Diagnostics of the fit iteration
+        (see radial_fitters.FrankFitter.fit)
     """
 
     from frank import radial_fitters
@@ -308,49 +308,49 @@ def output_results(u, v, vis, weights, sol, iteration_diag, iter_plot_range,
     Parameters
     ----------
     u, v : array, unit = :math:`\lambda`
-          u and v coordinates of observations
+        u and v coordinates of observations
     vis : array, unit = Jy
-          Observed visibilities (complex: real + imag * 1j)
+        Observed visibilities (complex: real + imag * 1j)
     weights : array, unit = Jy^-2
-          Weights assigned to observed visibilities, of the form
-          :math:`1 / \sigma^2`
+        Weights assigned to observed visibilities, of the form
+        :math:`1 / \sigma^2`
     sol : _HankelRegressor object
-          Reconstructed profile using Maximum a posteriori power spectrum
-          (see frank.radial_fitters.FrankFitter)
+        Reconstructed profile using Maximum a posteriori power spectrum
+        (see frank.radial_fitters.FrankFitter)
     iteration_diag : _HankelRegressor object
-          Diagnostics of the fit iteration
-          (see radial_fitters.FrankFitter.fit)
+        Diagnostics of the fit iteration
+        (see radial_fitters.FrankFitter.fit)
     iter_plot_range : list or None
-          Range of iterations in the fit over which to
-          plot brightness profile and power spectrum reconstructions. If None,
-          then the full range will be plotted.
+        Range of iterations in the fit over which to
+        plot brightness profile and power spectrum reconstructions. If None,
+        then the full range will be plotted.
     bin_widths : list, unit = \lambda
-          Bin widths in which to bin the observed visibilities
+        Bin widths in which to bin the observed visibilities
     save_dir : string
-          Directory in which output datafiles and figures are saved
+        Directory in which output datafiles and figures are saved
     uvtable_filename : string
-          Filename for observed UVTable. The saved datafiles and figures use
-          this as their filename prefix
+        Filename for observed UVTable. The saved datafiles and figures use
+        this as their filename prefix
     save_profile_fit : bool
-          Whether to save fitted brightness profile
+        Whether to save fitted brightness profile
     save_vis_fit : bool
-          Whether to save fitted visibility distribution
+        Whether to save fitted visibility distribution
     save_uvtables : bool
-          Whether to save fitted and residual UV tables.
-          NOTE: These are reprojected
+        Whether to save fitted and residual UV tables.
+        NOTE: These are reprojected
     save_iteration_diag : bool
-          Whether to save diagnostics of the fit iteration
+        Whether to save diagnostics of the fit iteration
     full_plot : bool
-          Whether to make a figure more fully showing the fit and its
-          diagnostics
+        Whether to make a figure more fully showing the fit and its
+        diagnostics
     quick_plot : bool
-          Whether to make a figure showing the simplest plots of the fit
+        Whether to make a figure showing the simplest plots of the fit
     diag_plot : bool
-          Whether to make a figure showing convergence diagnostics for the fit
+        Whether to make a figure showing convergence diagnostics for the fit
     force_style: bool, default = True
-          Whether to use preconfigured matplotlib rcParams in generated figures
+        Whether to use preconfigured matplotlib rcParams in generated figures
     dist : float, optional, unit = AU, default = None
-          Distance to source, used to show second x-axis for brightness profile
+        Distance to source, used to show second x-axis for brightness profile
     """
 
     logging.info('  Plotting results')
