76f90a00d622f48bc78631d356a6df3c42bcccd9
Batman (by day, Jeff Jennings) <jmj51@ast.cam.ac.uk>
2020-03-04 12:32:30 +0000

    minor typos

--- frank/radial_fitters.py
+++ frank/radial_fitters.py
@@ -653,7 +653,7 @@ class FrankFitter(FourierBesselFitter):
 
         return Tij * self._smooth
 
-    def fit(self, u, v, V, weights=1):
+    def fit(self, u, v, V, weights=1, verbose=False):
         r"""
         Fit the visibilties
 
@@ -722,7 +722,8 @@ class FrankFitter(FourierBesselFitter):
         pi_old = 0
         while (np.any(np.abs(pi - pi_old) > self._tol * pi) and
                count <= self._max_iter):
-            #print('\r    Fit iteration {}'.format(count), end='', flush=True)
+            if verbose:
+                print('\r    FrankFitter iteration {}'.format(count), end='', flush=True)
 
             # Project mu to Fourier-space
             #   Tr1 = Trace(mu mu_T . Ykm_T Ykm) = Trace( Ykm mu . (Ykm mu)^T)
