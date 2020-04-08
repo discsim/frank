444afd19ba67c51e776695bfd1d6e6b1303644f0
Richard Booth <richardabooth@gmail.com>
2020-04-08 10:03:23 +0100

    re-ordered check

--- frank/radial_fitters.py
+++ frank/radial_fitters.py
@@ -118,11 +118,12 @@ class _HankelRegressor(object):
 
         self._p = p
         if p is not None:
-            Ykm = self._DHT.coefficients()
             if np.any(p <= 0) or np.any(np.isnan(p)):
                 raise ValeuError("Bad value in power spectrum. The power"
                                  " spectrum must be postive and not contain"
                                  " any NaN values")
+            
+            Ykm = self._DHT.coefficients()
             self._Sinv = np.einsum('ji,j,jk->ik', Ykm, 1/p, Ykm)
         else:
             self._Sinv = None
