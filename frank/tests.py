fixed signs in tests

frank/tests.py
@@ -115,9 +115,9 @@ def test_fit_geometry():
 
     np.testing.assert_allclose([geom.PA, geom.inc, 1e3 * geom.dRA,
                                 1e3 * geom.dDec],
-                               [-1.4916013559412147 / deg_to_rad,
-                                0.5395904796783955 / deg_to_rad,
-                                0.6431627790617276, 1.161768824369382],
+                               [1.4916013559412147 / deg_to_rad,
+                                -0.5395904796783955 / deg_to_rad,
+                                -0.6431627790617276, -1.161768824369382],
                                err_msg="Gaussian geometry fit")
 
 
@@ -203,9 +203,9 @@ def test_fit_geometry_inside():
     geom = sol.geometry
     np.testing.assert_allclose([geom.PA, geom.inc, 1e3 * geom.dRA,
                                 1e3 * geom.dDec],
-                               [-1.4916013559412147 / deg_to_rad,
-                                0.5395904796783955 / deg_to_rad,
-                                0.6431627790617276, 1.161768824369382],
+                               [1.4916013559412147 / deg_to_rad,
+                                -0.5395904796783955 / deg_to_rad,
+                                -0.6431627790617276, -1.161768824369382],
                                err_msg="Gaussian geometry fit inside Frank fit")
 
 