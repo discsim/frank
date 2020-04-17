Corrected phase centre shift signs

frank/geometry.py
@@ -131,11 +131,13 @@ class SourceGeometry(object):
         Position angle of the disc
     dRA : float, unit = arcsec
         Phase centre offset in right ascension.
-        NOTE: The sign convention is xx
     dDec : float, units = arcsec
         Phase centre offset in declination.
-        NOTE: The sign convention is xx
 
+    Notes
+    -----
+    The phase centre offsets, dRA and dDec, refer to the distance to the source
+    from the phase centre.
     """
 
     def __init__(self, inc=None, PA=None, dRA=None, dDec=None):
@@ -167,7 +169,7 @@ class SourceGeometry(object):
             Corrected complex visibilites
 
         """
-        Vp = apply_phase_shift(u, v, V, self._dRA, self._dDec)
+        Vp = apply_phase_shift(u, v, V, self._dRA, self._dDec, inverse=True)
         up, vp = deproject(u, v, self._inc, self._PA)
 
         return up, vp, Vp
@@ -195,7 +197,7 @@ class SourceGeometry(object):
             Corrected complex visibilites
         """
         up, vp = self.reproject(u, v)
-        Vp = apply_phase_shift(up, vp, V, self._dRA, self._dDec, inverse=True)
+        Vp = apply_phase_shift(up, vp, V, self._dRA, self._dDec, inverse=False)
 
         return up, vp, Vp
 
@@ -264,11 +266,13 @@ class FixedGeometry(SourceGeometry):
         Disc positition angle.
     dRA : float, default = 0, unit = arcsec
         Phase centre offset in right ascension
-        NOTE: The sign convention is xx
     dDec : float, default = 0, unit = arcsec
         Phase centre offset in declination
-        NOTE: The sign convention is xx
 
+    Notes
+    -----
+    The phase centre offsets, dRA and dDec, refer to the distance to the source
+    from the phase centre.
     """
 
     def __init__(self, inc, PA, dRA=0.0, dDec=0.0):
@@ -288,6 +292,10 @@ class FitGeometryGaussian(SourceGeometry):
          phase_centre = None, the phase centre is fit for. Else the phase
          centre should be provided as a tuple
 
+    Notes
+    -----
+    The phase centre offsets, dRA and dDec, refer to the distance to the source
+    from the phase centre.
     """
 
     def __init__(self, phase_centre=None):
@@ -352,6 +360,11 @@ def _fit_geometry_gaussian(u, v, V, weights, phase_centre=None):
     fac = 2*np.pi / rad_to_arcsec
     w = np.sqrt(weights)
 
+    if phase_centre is not None:
+        dRA, dDec = phase_centre
+        phi = dRA*fac * u + dDec*fac * v
+        Vp = V * (np.cos(phi) - 1j*np.sin(phi))
+
     def wrap(fun):
         return np.concatenate([fun.real, fun.imag])
 
@@ -360,9 +373,8 @@ def _fit_geometry_gaussian(u, v, V, weights, phase_centre=None):
 
         if phase_centre is None:
             phi = dRA*fac * u + dDec*fac * v
-            Vp = V * (np.cos(phi) + 1j*np.sin(phi))
-        else:
-            Vp = V
+            Vp = V * (np.cos(phi) - 1j*np.sin(phi))
+
 
         c_t = np.cos(pa)
         s_t = np.sin(pa)