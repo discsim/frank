1fc679ba1b3ee43068a4bb7eafe24a1bdfde4144
Richard Booth <richardabooth@gmail.com>
2020-04-30 17:44:44 +0100

    removed " from string

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 with open("frank/__init__.py", "r") as f:
     for line in f:
         if line.startswith('__version__'):
-            version = line.split('=')[1].strip()
+            version = line.split('=')[1].strip().strip('"')
 
 setup(
     name="frank",
