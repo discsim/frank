9ee3900e9d8a280aa078ccfd4e76c108daf6f9b2
Richard Booth <richardabooth@gmail.com>
2020-06-15 13:58:17 +0100

    More verbose error

--- frank/io.py
+++ frank/io.py
@@ -66,7 +66,7 @@ def load_uvtable(data_file):
         if not np.iscomplexobj(vis):
             raise ValueError("You provided a UVTable with the extension {}."
                              " This extension requires the UVTable's variable 'V' to be"
-                             " complex (of the form xx + yy * 1j).".format(extension))
+                             " complex (of the form Re(V) + Im(V) * 1j).".format(extension))
                 
     else:
         raise ValueError("You provided a UVTable with the extension {}."
