daa8385e13207a105405e20cdc3c2d5cddaa02a2
jeffjennings <jmj51@ast.cam.ac.uk>
2020-08-12 07:38:36 -0800

    _fit_geometry_gaussian: remove case for different units

--- frank/geometry.py
+++ frank/geometry.py
@@ -492,10 +492,7 @@ def _fit_geometry_gaussian(u, v, V, weights, guess, inc_pa=None,
     if phase_centre is not None:
         dRA, dDec = phase_centre
 
-    if inc_pa is None:
-        geometry = inc / deg_to_rad, PA / deg_to_rad, dRA, dDec
-    else:
-        geometry = inc, PA, dRA, dDec
+    geometry = inc / deg_to_rad, PA / deg_to_rad, dRA, dDec
 
     return geometry
 
