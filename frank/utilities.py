16feaa09a8d3e456bea5e48ec5b7586c50098c64
jeffjennings <jjennings1519@gmail.com>
2022-08-04 15:07:36 -0400

    fix docs build warning

--- frank/utilities.py
+++ frank/utilities.py
@@ -456,6 +456,7 @@ def estimate_weights(u, v=None, V=None, nbins=300, log=True, use_median=False):
         In each case the variance of V in the uv-bins is used to estimate the
         weights. The first call will use q = np.hypot(u, v) in the uv-bins. The
         second and third calls are equivalent to the first with u=0.
+        
     """
 
     logging.info('  Estimating visibility weights')
