271ce6b30aa055126330fe5c30b61a8cea2fc522
Jeff Jennings <jjennings1519@gmail.com>
2023-11-02 12:39:09 -0400

    tests: get_collocation_points (backward)

--- frank/tests.py
+++ frank/tests.py
@@ -626,6 +626,14 @@ def test_get_collocation_points():
        1.07005922, 1.25602496, 1.44200623, 1.62799772, 1.8139963 ]
     np.testing.assert_allclose(result, expected, rtol=2e-5, atol=1e-8)
 
+    # call with backward DHT
+    result_bwd = utilities.get_collocation_points(N=10, direction='backward')
+    expected_bwd = [ 39472.88305737,  90606.73736504, 142042.56471889, 193546.62066389,
+       245076.55732463, 296619.01772663, 348168.47711355, 399722.24089812,
+       451278.83939289, 502837.4032234 ]
+    np.testing.assert_allclose(result_bwd, expected_bwd, rtol=2e-5, atol=1e-8)    
+    
+
 def _run_pipeline(geometry='gaussian', fit_phase_offset=True,
                    fit_inc_pa=True, make_figs=False,
                    multifit=False, bootstrap=False):
